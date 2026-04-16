8a7e7cf28d177226de2b7ddf40fa341c552cb88e
100495829 <100495829@alumnos.uc3m.es>
2026-04-16 11:24:52 +0200

    api en docker solucionada

--- backend/newsradar_api/app/database_mongodb.py
+++ backend/newsradar_api/app/database_mongodb.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Conexión a Mongo. Por defecto a localhost para desarrollo local.
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
 
 client = AsyncIOMotorClient(MONGO_URL)
 db_mongo = client.newsradar_archive
